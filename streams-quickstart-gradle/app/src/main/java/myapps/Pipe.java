e50a1270e4a5397b3104e9f3249c102199633d89
Leonardo Cantarella <leonardo@cantarella.it>
2024-04-27 19:24:23 +0200

    simplest stream

---
+++ streams-quickstart-gradle/app/src/main/java/myapps/Pipe.java
@@ -0,0 +1,28 @@
+package myapps;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import java.util.Properties;
+
+/**
+ * Pipe
+ */
+public class Pipe {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> source = builder.stream("cars");
+        source.to("testout");
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));      
+    }
+    
+}
